6692fd3f614a6e568491998e09f38dc10f3daa96
grumpymiki <ohujah1@gmail.com>
2026-03-21 13:03:39 +0100

    usuwanie ostatnich zalegajacych obrazkow
    ??

---
+++ src/assets/icons/index.js
@@ -0,0 +1,20 @@
+//barrel file dla obrazkow komponentow reacta
+
+export { default as AboutUs } from './aboutUs.svg';
+export { default as AboutUsWhite } from './aboutUs_white.svg';
+export { default as AccordionOpen } from './accordionOpen.svg';
+export { default as Buttons } from './buttons.svg';
+export { default as Contact } from './contact.svg';
+export { default as ContactWhite } from './contact_white.svg';
+export { default as Dpad } from './dpad.svg';
+export { default as Envelope } from './envelope.svg';
+export { default as Home } from './home.svg';
+export { default as HomeWhite } from './home_white.svg';
+export { default as Joystick } from './joystick.svg';
+export { default as Products } from './products.svg';
+export { default as ProductsWhite } from './products_white.svg';
+export { default as Rounded } from './rounded.svg';
+export { default as Star } from './star.svg';
+export { default as Star2 } from './star2.svg';
+export { default as Tap } from './tap.svg';
+export { default as Twotone } from './twotone.svg';
